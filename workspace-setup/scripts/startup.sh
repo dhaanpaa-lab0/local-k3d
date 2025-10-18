cf90e06d20852824477c2c6419cad09644f61010
Daniel Haanpaa <dhaanpaa@djhent.us>
2025-10-17 20:22:42 -0700

    Added Workspace Container Setup

---
+++ workspace-setup/scripts/startup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "---------------- WORKSPACE SETUP --------------"
+echo "                    SETUP                      "
+echo "-----------------------------------------------"
+
+ls -alh /wrk
